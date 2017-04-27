c61228f2afe85242cdb3d71ca69d5564ab480099
Heesu Suh <hssuh96@gmail.com>
2017-04-27 14:32:48 +0900

    change content-script injection style

--- background/background.js
+++ background/background.js
@@ -5,22 +5,3 @@ chrome.storage.sync.get(function(obj) {
     chrome.storage.sync.set(defaultStorage);
   }
 });
-
-chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-  console.log('onDOMContentLoaded fired');
-  console.log(details);
-
-  if (details.url.indexOf('ajax') === -1 && details.url.indexOf('php') === -1) {
-    console.log('onDOMContentLoaded fired and condition matched');
-    console.log(details);
-    chrome.tabs.executeScript(details.tabId, {file: "libs/jquery-3.1.1.min.js"}, function() {
-      chrome.tabs.executeScript(details.tabId, {file: "libs/arrive.min.js"}, function() {
-        chrome.tabs.executeScript(details.tabId, {file: "content/change-contents.js"});
-      });
-    });
-  }
-}, {
-  url: [
-    {hostContains: 'www.facebook.com'}
-  ]
-});
